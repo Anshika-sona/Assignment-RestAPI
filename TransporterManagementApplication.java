ebe7ff7a26e56e6ca9ff1f40cd8d5abcb23db263
Anshika <97749793+Anshika-sona@users.noreply.github.com>
2024-05-12 14:50:09 +0530

    Create TransporterManagementApplication.java

---
+++ TransporterManagementApplication.java
@@ -0,0 +1,12 @@
+package com.liveasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporterManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransporterManagementApplication.class, args);
+    }
+}
